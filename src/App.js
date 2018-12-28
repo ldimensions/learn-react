abd50cd721508c5e20da7455973f0676c8a021df
Lindo Sebastian <lindosebastian@gmail.com>
2018-12-27 19:03:13 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -10,12 +10,30 @@ class App extends Component {
     ]
   }
 
+  clickHandler = (val) => {
+    this.setState({Person:[{name: val}]});
+  }  
+  clickHandler1 = () => {
+    this.setState({Person:[{name: "No Name..."}]});
+  } 
+  nameChangedhandler = (event) =>{
+    this.setState({Person:[{name: event.target.value}]});
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-            Learn React  
-            <Person name={this.state.Person[0].name}>Children Elements</Person>       
+            <Person 
+              clickEvent={this.clickHandler.bind(this,'testing')} 
+              name={this.state.Person[0].name}
+              inputHandler={this.nameChangedhandler}
+            >
+              Children Elements
+            </Person>   
+            <button onClick={this.clickHandler.bind(this,'manuuuuu')}> Click - binding method 1 with parameter</button> 
+            <button onClick={ () => this.clickHandler('manuuuuu111')}> Click - binding method 2 with parameter (not recommended)</button>  {/* This might re render*/}
+            <button onClick={this.clickHandler1}> Click without parameter</button>   
         </header>
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,9 +3,11 @@ import React from 'react';
 const person = (props) => {
     return (
         <div>
-            <p>Person Component</p>
+            <p>Person stateless Component</p>
             <p>Name: {props.name}</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.inputHandler} value={props.name}/>
+            <p onClick={props.clickEvent}> Click here to change the name - child stateless method call</p>
         </div>
     )
 }
